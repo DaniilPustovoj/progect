c4ba891a4f3dca940f38a2dc598a942b9fd06efb
Daniil <pustovoj.daniil2018@gmail.com>
2019-07-16 15:51:08 +0300

    first commit

---
+++ huj.js
@@ -0,0 +1,60 @@
+//"типы данных"
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+var number = 10;
+var string = "Hello";
+var sym = Symbol;
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(2 / 0);
+console.log(string* number);
+
+let something;
+
+console.log(something);
+
+let persone = {
+    name: "Bob",
+    age: 25,
+    isMarried: false
+};
+
+let arr = ['her.jpg', 'hyi.png', 'rofl.jpg'];
+
+console.log(arr[1]);
+
+let flo = ['film.fog', 'grot.gryt'];
+
+console.log(flo[0]);
+
+//alert("Hello World");
+
+//let answer = confirm("ты крут");
+
+//console.log(answer);
+
+//let answer = prompt('Есть ли вам 18?', 'DA');
+
+//console.log(typeof(null));
+
+//console.log(4 + " - chislo");
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked && !isClose);
